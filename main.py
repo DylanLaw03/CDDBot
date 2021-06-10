6fb4029431a0348cc2844e3bca7fc1a5fd10e722
Dylan <Dylan>
2021-06-09 22:40:05 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+This program will scrape a page from the website cheap digital download, to find potentially profiatble games.
+Started on 6/9/21
+'''
+
+def main ():
+    pass
+
+def open_page(url):
+    
